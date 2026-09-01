6519da36db4bf8b60445224321247b784efe8fbb
Anachr0nistic <novaify08@gmail.com>
2026-09-01 12:22:29 +0200

    commit

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-#test 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main_route():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port="8081")
