b68eeba98d2b5a58956d4e9ef47b176451bf1b1f
Anachr0nistic <novaify08@gmail.com>
2026-09-11 13:06:55 +0200

    new login page

--- app.py
+++ app.py
@@ -15,5 +15,8 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
 if __name__ == "__main__":
     app.run(debug=True, port="8081")
