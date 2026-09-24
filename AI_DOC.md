51637d6a9bcad993afc296b08af8a57add9b4a4c
Anachr0nistic <novaify08@gmail.com>
2026-09-24 21:25:17 +0200

    login progress--shit sitll broken tho

--- AI_DOC.md
+++ AI_DOC.md
@@ -2,5 +2,4 @@
 ## CSS
 Et problem med CSS, hvor mit mainwindow udstrækker sig ud fra viewporten. 
 PROMPT: "jeg har et problem, hvori min mainwindow går uden for min viewport" *konteskt kode gives*
-Løsningsforslag: erstat body margin med body padding  og tilføj box-sizing: border-box;
-Erstat også min-width: 100%; med flex: 1; min-width: 0;
+Løsningsforslag: height: calc(100vh-30px), hvor 30px er margin og 100vh er viewport højden.
--- app.py
+++ app.py
@@ -17,31 +17,37 @@ def index():
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    username = str(request.form.get("username"))
-    password = str(request.form.get("password"))
-    print(username, password)
-    print(c.execute('''SELECT * FROM USERS WHERE USERNAME = (?) ''', (username,)).fetchall())
-    with conn:
-        if username == c.execute('''SELECT * FROM USERS WHERE USERNAME = (?) ''', (username,)).fetchall():
-            print("yippie")
+    if request.method == "POST":
+        username = str(request.form.get("username"))
+        password = str(request.form.get("password"))
+        print(username, password)
+        #print(c.execute('''SELECT * FROM USERS''').fetchall())
+        table = c.execute('''SELECT * FROM USERS WHERE USERNAME = (?)''', username).fetchall()
+        print(table)
+        if username == table[1]:
+            print("username passed")
         else:
-            print("FUCK")
+            print("spell ur name correctly dumbass")
+        if password == table[2]:
+            print("password passed")
+        else:
+            print("wrong nomber")
 
-        #c.execute('''SELECT * FROM USERS WHERE PASSWORD = (?) ''', (password)).fetchall()
+
+            #c.execute('''SELECT * FROM USERS WHERE PASSWORD = (?) ''', (password)).fetchall()
     return render_template("login.html")
-@app.route("/signup", methods=["POST"])
+@app.route("/signup", methods=["POST", "GET"])
 def signup():
-    signup_name = request.form.get("signup_name")
-    password1 = request.form.get("password1")
-    password2 = request.form.get("password2")
-    params = (signup_name, password1)
-    if password1 == password2:
-        with conn:
-            c.execute("INSERT INTO USERS (ID, USERNAME, PASSWORD) VALUES (NULL, ?, ?)", params)
-    elif password1 != password2:
-        pass #GØR MÅSKE, SÅ EN SIGN-UP FEJL BESKED POPPER OP HVIS JEG HAR TID
-
-
+    if request.method == "POST":
+        signup_name = request.form.get("signup_name")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        params = (signup_name, password1)
+        if password1 == password2:
+            with conn:
+                c.execute('''INSERT INTO USERS (ID, USERNAME, PASSWORD) VALUES (NULL, ?, ?)''', params)
+        elif password1 != password2:
+            pass #GØR MÅSKE, SÅ EN SIGN-UP FEJL BESKED POPPER OP HVIS JEG HAR TID
     return render_template("signup.html")
 if __name__ == "__main__":
     app.run(debug=True, port="8082")
